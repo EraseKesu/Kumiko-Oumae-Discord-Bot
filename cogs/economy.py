3d4f45c5e802c9f1d8f08b3b2aba1de8f65b2460
EraseKesu <eitan.olchik@gmail.com>
2020-05-13 10:04:36 +0100

    make it better

--- cogs/economy.py
+++ cogs/economy.py
@@ -18,10 +18,10 @@ async def get_prefix(bot, message):
                                   message.guild.id
                                   )
     if res is None:
-        prefix = commands.when_mentioned_or('+-')
+        prefix = '+-'
 
     if res is not None:
-        prefix = commands.when_mentioned_or(res.get("prefix"))
+        prefix = res.get("prefix")
 
     return prefix
 
